54cacad1868207df1979b4feaad1dd897713fb03
mariks1 <marat-khasanshin@list.ru>
2022-10-23 21:52:10 +0300

    add src

---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+
+import pokemons.*;
+import ru.ifmo.se.pokemon.*;
+
+public class main {
+    public main() {
+    }
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        b.addAlly(new Girafarig("Серега", 1));
+        b.addAlly(new Gallade("Андрейчмо", 1));
+        b.addAlly(new Kirlia("фен", 1));
+        b.addFoe(new Pansage("меф", 1));
+        b.addFoe(new Ralts("исаак ньютон", 1));
+        b.addFoe(new Simisage("арбитраж криптовалютович", 1));
+        b.go();
+    }
+}
